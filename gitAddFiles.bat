1c4b4d1d3d24f43886af0e1d7b43ccc4cc7619fe
peter <peter.niederer@sonnenkinder.org>
2020-11-05 02:19:32 +0100

    starting...

--- gitAddFiles.bat
+++ gitAddFiles.bat
@@ -5,8 +5,9 @@ echo allways the same files to add, so best put it in a batch file,
 echo needed since this somewhat strange ´staged-to-commit´ feature had been added
 echo .
 echo 
-git add ST/tempix-03/*
+git add ST/tempix/*
 git add ST/STM32F767ZI_Nucleo/*
+git add Micrium/*
 echo do not git add ST/BSP/STM32F767ZI_Nucleo/stm32f7xx_hal_conf.h
 
 git add .gitignore
