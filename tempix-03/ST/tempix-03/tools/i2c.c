c637e5316f4dc2319b7f740512d17301a140135a
peetz <peter.niederer@protonmail.com>
2021-10-21 16:18:41 +0200

    reg save

--- tempix-03/ST/tempix-03/tools/i2c.c
+++ tempix-03/ST/tempix-03/tools/i2c.c
@@ -248,9 +248,9 @@ void establishContactAndRun()
 
 	i2cTransferConfig(&hi2c1,i2cJobData.address,i2cJobData.amtChars,(i2cJobData.jobType == receiveI2c ? 1:0));
 	hi2c1.Instance->TXDR = (i2cJobData.address << 1);
-	if (i2cJobData.jobType == receiveI2c) {
-		hi2c1.Instance->TXDR |= 0x01;
-	}  // did also not work ..... ???????
+//	if (i2cJobData.jobType == receiveI2c) {
+//		hi2c1.Instance->TXDR |= 0x01;
+//	}  // did also not work ..... ???????
 	i2cSendStart(&hi2c1);
 }
 
@@ -427,14 +427,172 @@ void initI2cTo4()
 	// methods intended for debugging nasty i2c problem
 
 	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	 RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
 	I2C_HandleTypeDef hi2c2;
 	I2C_HandleTypeDef hi2c3;
 	I2C_HandleTypeDef hi2c4;
 
+
+
+	  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_I2C2|RCC_PERIPHCLK_I2C3
+	                              |RCC_PERIPHCLK_I2C4;
+	  PeriphClkInitStruct.I2c2ClockSelection = RCC_I2C2CLKSOURCE_PCLK1;
+	  PeriphClkInitStruct.I2c3ClockSelection = RCC_I2C3CLKSOURCE_PCLK1;
+	  PeriphClkInitStruct.I2c4ClockSelection = RCC_I2C4CLKSOURCE_PCLK1;
+	  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+	  {
+	    while(1){};
+	  }
+
+
+
+	 __HAL_RCC_GPIOF_CLK_ENABLE();
+	  __HAL_RCC_GPIOH_CLK_ENABLE();
+	  __HAL_RCC_GPIOC_CLK_ENABLE();
+	  __HAL_RCC_GPIOA_CLK_ENABLE();
+
 	__HAL_RCC_I2C2_CLK_ENABLE();
 	__HAL_RCC_I2C3_CLK_ENABLE();
 	__HAL_RCC_I2C4_CLK_ENABLE();
+
+    __HAL_RCC_GPIOF_CLK_ENABLE();
+    /**I2C2 GPIO Configuration
+    PF0     ------> I2C2_SDA
+    PF1     ------> I2C2_SCL
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C2;
+    HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C2_CLK_ENABLE();
+
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**I2C3 GPIO Configuration
+    PC9     ------> I2C3_SDA
+    PA8     ------> I2C3_SCL
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C3;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_8;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C3;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C3_CLK_ENABLE();
+
+
+    __HAL_RCC_GPIOF_CLK_ENABLE();
+    /**I2C4 GPIO Configuration
+    PF14     ------> I2C4_SCL
+    PF15     ------> I2C4_SDA
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_I2C4;
+    HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C4_CLK_ENABLE();
+
+
+	  hi2c2.Instance = I2C2;
+	  hi2c2.Init.Timing = 0x20404768;
+	  hi2c2.Init.OwnAddress1 = 0;
+	  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+	  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+	  hi2c2.Init.OwnAddress2 = 0;
+	  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+	  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+	  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+	  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+	  {
+		  while(1){};
+	  }
+	  /** Configure Analogue filter
+	  */
+	  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+	  {
+		  while(1){};
+	  }
+	  /** Configure Digital filter
+	  */
+	  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
+	  {
+		  while(1){};
+	  }
+
+
+
+	  hi2c3.Instance = I2C3;
+	  hi2c3.Init.Timing = 0x20404768;
+	  hi2c3.Init.OwnAddress1 = 0;
+	  hi2c3.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+	  hi2c3.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+	  hi2c3.Init.OwnAddress2 = 0;
+	  hi2c3.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+	  hi2c3.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+	  hi2c3.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+	  if (HAL_I2C_Init(&hi2c3) != HAL_OK)
+	  {
+		  while(1){};
+	  }
+	  /** Configure Analogue filter
+	  */
+	  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c3, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+	  {
+		  while(1){};
+	  }
+	  /** Configure Digital filter
+	  */
+	  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c3, 0) != HAL_OK)
+	  {
+		  while(1){};
+	  }
+
+
+	  hi2c4.Instance = I2C4;
+	  hi2c4.Init.Timing = 0x20404768;
+	  hi2c4.Init.OwnAddress1 = 0;
+	  hi2c4.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+	  hi2c4.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+	  hi2c4.Init.OwnAddress2 = 0;
+	  hi2c4.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+	  hi2c4.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+	  hi2c4.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+	  if (HAL_I2C_Init(&hi2c4) != HAL_OK)
+	  {
+		  while(1){};
+	  }
+	  /** Configure Analogue filter
+	  */
+	  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c4, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+	  {
+		  while(1){};
+	  }
+	  /** Configure Digital filter
+	  */
+	  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c4, 0) != HAL_OK)
+	  {
+		  while(1){};
+	  }
+
 }
 
 
